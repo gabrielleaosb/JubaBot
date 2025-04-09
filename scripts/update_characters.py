6217e4812c2318edc45b45697611cf15731f89d2
gabrielleaosb <gabriellsbagetti@gmail.com>
2025-04-09 15:02:50 -0300

    sw

--- scripts/update_characters.py
+++ scripts/update_characters.py
@@ -2,7 +2,6 @@ import sys
 import os
 import asyncio
 
-# Garante que o Python ache a pasta raíz
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from database.insert_data import insert_characters
